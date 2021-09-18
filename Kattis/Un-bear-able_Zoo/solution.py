1ad8d04ed7dab8a4d46cc3667ac659dc0b3a56ec
JonathanStarup <32037926+JonathanStarup@users.noreply.github.com>
2021-09-18 19:48:37 +0200

    output

--- Kattis/Un-bear-able_Zoo/solution.py
+++ Kattis/Un-bear-able_Zoo/solution.py
@@ -1,5 +1,6 @@
 from sys import stdin
 lines = stdin.read().splitlines()
+output = []
 
 line_index = 0
 test_n = 1
@@ -9,19 +10,20 @@ while 1:
     line_index += 1
     if n == 0:
         break
-    
+
     animals = {}
     for animal_index in range(line_index, line_index + n):
         animal = lines[animal_index].split()[-1].lower()
-        current = animals[animal] if animal in animals else 0
-        animals[animal] = current + 1
+        animals[animal] = animals.get(animal, 0) + 1
     
-    print(f'List {test_n}:')
+    output.append(f'List {test_n}:')
     items = list(animals.items())
     items.sort()
     for animal_t in items:
         animal, count = animal_t
-        print(f'{animal} | {count}')
+        output.append(f'{animal} | {count}')
 
     line_index += n
     test_n += 1
+
+print("\n".join(output))
