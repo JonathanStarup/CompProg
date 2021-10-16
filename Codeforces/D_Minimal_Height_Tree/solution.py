3962d668d8776d36bd7c44e12f52c83c9292e2c2
jonathanStarup <32037926+JonathanStarup@users.noreply.github.com>
2021-10-16 18:15:12 +0200

    setup

---
+++ Codeforces/D_Minimal_Height_Tree/solution.py
@@ -0,0 +1,5 @@
+import sys
+INF = float('inf')
+input = iter(sys.stdin.read().splitlines())
+def iinput(): return map(int, next(input).split())
+def debug(*ss): print("  >"+" ".join(map(lambda s:f"{s:>8} = {globals()[s] if s in globals() else locals()[s]:<10}", ss)))
