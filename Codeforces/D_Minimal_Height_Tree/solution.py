b4010a3808abd8a5b151f6b7d67e396381bdca23
jonathanStarup <32037926+JonathanStarup@users.noreply.github.com>
2021-10-17 16:46:08 +0200

    wip

--- Codeforces/D_Minimal_Height_Tree/solution.py
+++ Codeforces/D_Minimal_Height_Tree/solution.py
@@ -1,5 +1,27 @@
 import sys
-INF = float('inf')
+from collections import deque
+#INF = float('inf')
 input = iter(sys.stdin.read().splitlines())
-def iinput(): return map(int, next(input).split())
-def debug(*ss): print("  >"+" ".join(map(lambda s:f"{s:>8} = {globals()[s] if s in globals() else locals()[s]:<10}", ss)))
+def iinput(): return list(map(int, next(input).split()))
+#def debug(*ss): print("  >"+" ".join(map(lambda s:f"{s:>8} = {globals()[s] if s in globals() else locals()[s]:<10}", ss)))
+
+T = int(next(input))
+for _ in range(T):
+    N = int(next(input))
+    bfs = list(iinput())
+    parents = deque([0])
+    group_size = 0
+    reset = True
+    for i in range(1, N):
+        if reset:
+            reset = False
+            group_size += 1
+            continue
+        if bfs[i] < bfs[i-1]:
+            reset = True
+            level = parents.popleft() + 1
+            parents.extend([level]*group_size)
+            group_size = 0
+
+
+    print(height)
--- python_notes.py
+++ python_notes.py
@@ -2,7 +2,7 @@
 import sys
 INF = float('inf')
 input = iter(sys.stdin.read().splitlines())
-def iinput(): return map(int, next(input).split())
+def iinput(): return list(map(int, next(input).split()))
 def debug(*ss): print("  >"+" ".join(map(lambda s:f"{s:>8} = {globals()[s] if s in globals() else locals()[s]:<10}", ss)))
 
 # infinite
